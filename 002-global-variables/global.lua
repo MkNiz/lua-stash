fb8b558d98578f123fecbdb8824b5caa4c452ec4
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-09 14:55:31 -0400

    002 - Global Variables

---
+++ 002-global-variables/global.lua
@@ -0,0 +1,15 @@
+print("An undeclared variable will be treated as:")
+print(i_have_not_been_declared)
+print("")
+
+my_var = 20
+
+print("The declared variable has a value of:")
+print(my_var)
+print("")
+
+my_var = nil
+
+print("Until we set it to nil, at which point it will be:")
+print(my_var)
+print("And its memory can be reclaimed.")
---
+++ 002-global-variables/readme.md
@@ -0,0 +1,4 @@
+# Global Variables
+
+Demonstrates how Lua handles variables, specifically when they are defined
+in the global scope
