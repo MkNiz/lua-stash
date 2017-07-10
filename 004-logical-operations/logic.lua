7b2be0ca27d149cea7920c8c8037cff1a0e1fa42
Michael Nizzari <michael.nizzari@gmail.com>
2017-07-10 10:46:23 -0400

    Added 'not'

--- 004-logical-operations/logic.lua
+++ 004-logical-operations/logic.lua
@@ -12,3 +12,11 @@ print(1 or -20)
 print(nil or "Knuckles")
 print("Knuckles" or false)
 print(nil or false)
+print("")
+
+-- "not" passes a boolean representing the reverse of the value's truthiness
+print("not:")
+print(not 1)
+print(not "Knuckles")
+print(not true)
+print(not false)
