f8eb154f4c81657a00d8c098fc82082fbfda866c
Michael Nizzari <michael.nizzari@gmail.com>
2017-09-02 13:56:12 -0400

    025 - Write

---
+++ 025-write/readme.md
@@ -0,0 +1,3 @@
+# Write
+
+Demonstrates writing a file
---
+++ 025-write/write.lua
@@ -0,0 +1,10 @@
+file = io.open("wrote_this.txt", "w")
+file:write(
+[[
+Hello, I made this
+file using a single
+write command.
+]]
+)
+
+print("Made file 'wrote_this.txt'.")
